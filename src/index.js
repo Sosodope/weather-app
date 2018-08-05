653db6bdf5908cfb5bd4b27b1fba8a51f4965d35
Sosodope <sonadolkar93@hotmail.com>
2018-08-05 15:59:12 +0700

    remove service worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
